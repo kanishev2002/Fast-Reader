46910bd778aee6bf7ba54c0c0dbdff3a0361ef88
Ilya_Kanishev <kanishev2002@gmail.com>
2018-12-05 13:31:02 +0300

    Initial Commit

---
+++ Fast Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fast Reader
+//
+//  Created by Илья Канищев on 05/12/2018.
+//  Copyright © 2018 Илья Канищев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
