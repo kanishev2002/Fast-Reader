561aec860b497786363178b92e8beb0e074b0164
Ilya_Kanishev <kanishev2002@gmail.com>
2018-12-28 13:39:40 +0300

    Initial release. Added copy and paste screen

---
+++ Fast Reader/FastReaderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FastReaderViewController.swift
+//  Fast Reader
+//
+//  Created by Илья Канищев on 27/12/2018.
+//  Copyright © 2018 Илья Канищев. All rights reserved.
+//
+
+import UIKit
+
+class FastReaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
